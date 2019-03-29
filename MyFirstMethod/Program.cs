f03fee1c90b89920df58974e325d86ade1eaf381
Vitor Daniel <vitor.daniel@outlook.pt>
2019-03-29 09:48:56 +0000

    EX01 - 1º metodo

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,9 +4,15 @@ namespace MyFirstMethod
 {
     class Program
     {
+        static void CountTo10()
+        {
+            for(int i = 1 ; i <= 10; i++)
+                Console.WriteLine(i);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CountTo10();
+            CountTo10();
         }
     }
 }
