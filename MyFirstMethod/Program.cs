5fe3db0da6f0233a9204f7b5007ce5d637af4d15
Vitor Daniel <vitor.daniel@outlook.pt>
2019-03-29 11:27:07 +0000

    EX03 - XML comments

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -2,17 +2,31 @@
 
 namespace MyFirstMethod
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// method that changes function to CountToN(10)
+        /// </summary>
         static void CountTo10()
         {
             CountToN(10);
         }
+        /// <summary>
+        /// method that counts numbers from one to n; n being a designated number
+        /// </summary>
+        /// <param name="n">n is a number that changes each time it is printed</param>
         static void CountToN(int n)
         {
             for (int i = 1; i <= n; i++)
                 Console.WriteLine(i);
         }
+        /// <summary>
+        /// print the numbers from the previous methods twice for each method
+        /// </summary>
+        /// <param name="args">main does not return anything</param>
         static void Main(string[] args)
         {
             CountTo10();
